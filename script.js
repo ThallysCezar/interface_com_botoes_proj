c4bd833e12ac2d66e498fc3592ec8a23e0a36a00
ThallysCezar <75546628+ThallysCezar@users.noreply.github.com>
2021-12-21 17:45:18 -0300

    Refactor: files to js and css

---
+++ script.js
@@ -0,0 +1,13 @@
+//function displays text message
+let m1 = 'Este botão faz com que o LED ligue'
+let m2 = ' Este botão faz com que o LED desligue'
+
+
+//function prompts for name, then displays message
+function on() {
+    alert('LED LIGADO');
+}
+//function computes factorial of integer number
+function off() {
+    alert('LED DESLIGADO');
+}   
